38472913e8699f4e54b0110d2e5f75c3aa5d4ebc
Oleg Chumin <tschumin.oleg@gmail.com>
2023-01-30 15:19:42 +0300

    Add new files

--- src/main/java/spring_introduction/Test2.java
+++ src/main/java/spring_introduction/Test2.java
@@ -1,7 +1,31 @@
 package spring_introduction;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test2 {
     public static void main(String[] args) {
+        // creating Spring container
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet petCat = context.getBean("petCat", Pet.class); // used reflection
+        petCat.say();
+
+        Pet petDog = context.getBean("petDog", Pet.class); // used reflection
+        petDog.say();
+
+        Pet petDuck =  context.getBean("petDuck", Pet.class);
+        petDuck.say();
+
+        Pet petElephant = context.getBean("petElephant", Pet.class);
+        petElephant.say();
+
+        Pet petFrog = context.getBean("petFrog", Pet.class);
+        petFrog.say();
+
+        Pet petGiraffe = context.getBean("petGiraffe", Pet.class);
+        petGiraffe.say();
+
+        Pet petHorse = context.getBean("petHorse", Pet.class);
+        petHorse.say();
 
     }
 }
