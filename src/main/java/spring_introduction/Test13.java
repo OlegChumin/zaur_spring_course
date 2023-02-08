fccbff5e6fbe219d46539441d6277a0874126a64
Oleg <tschumin.oleg@gmail.com>
2023-02-08 12:01:04 +0300

    Add new files and changes

--- src/main/java/spring_introduction/Test13.java
+++ src/main/java/spring_introduction/Test13.java
@@ -2,14 +2,12 @@ package spring_introduction;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test12 {
+public class Test13 {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext8.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext9.xml");
         Dog2 myDog = context.getBean("petDog", Dog2.class);
         myDog.say();
-        Dog2 newDog = context.getBean("petDog", Dog2.class);
-        newDog.say(); //singleton
         System.out.println("Context is closing");
-        context.close();
+        context.close(); //destroy method for prototype will not be called!
     }
 }
