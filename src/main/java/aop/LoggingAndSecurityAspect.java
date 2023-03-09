1683f5fac4179e8f3e5106c1a1cdc88b4c7a22bd
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-09 17:18:07 +0300

    Ad new files

--- src/main/java/aop/LoggingAndSecurityAspect.java
+++ src/main/java/aop/LoggingAndSecurityAspect.java
@@ -1,9 +1,29 @@
 package aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAndSecurityAspect {
+    @Pointcut("execution(* get*())") // Pointcut - выражение в виде шаблона - метода на который ссылаются Аспекты ниже
+    //в данном случае аспекты выполняются для методов со всеми модификаторами доступа
+    // с любыми типами возвращаемых аргументов или void
+    // для всех методоа с get названии и без параметров
+    private void allGetMethods() {
+    }
+
+    @Before("allGetMethods()") //аннотация выполнения Aspect логгирования до выполнение метода из UniLibrary
+    public void beforeGetLoggingAdvice() {
+        System.out.println("beforeGetLoggingAdvic: попытка получить книгу/журнал");
+    }
+
+    @Before("allGetMethods()") //аннотация выполнения Aspect проверки безопасности до выполнение метода из UniLibrary
+    public void beforeGetSecurityAdvice() {
+        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
+    }
+
 
 }
---
+++ src/main/java/aop/UniLibrary.java
@@ -0,0 +1,14 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniLibrary {
+    public void getBook() {
+        System.out.println("Мы берем книгу из UniLibrary");
+    }
+
+    public void getMagazine() {
+        System.out.println("Мы берем журнал из UniLibrary");
+    }
+}
