c81cffdc0dcceef2a519852a48ce1c8a9fe87678
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-11 08:28:00 +0300

    Ad new files

--- src/main/java/aop_31_joint_point/Test_AOP_JointPoint.java
+++ src/main/java/aop_31_joint_point/Test_AOP_JointPoint.java
@@ -2,12 +2,14 @@ package aop_31_joint_point;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Test_AOP_Order {
+public class Test_AOP_JointPoint {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
+        Book book = context.getBean("book", Book.class);
         uniLibrary.getBook();
-        uniLibrary.getMagazine();
+        uniLibrary.addBook("Zaur", book);
+        uniLibrary.addMagazine();
         context.close();
     }
 }
--- src/main/java/aop_31_joint_point/aspects/LoggingAspect.java
+++ src/main/java/aop_31_joint_point/aspects/LoggingAspect.java
@@ -2,16 +2,25 @@ package aop_31_joint_point.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.aspectj.lang.JoinPoint;
 
 @Component
 @Aspect
 @Order(1)
 public class LoggingAspect {
-    @Before("aop_31_joint_point.MyPointcuts.allAddMethods()") //аннотация выполнения Aspect логгирования до выполнение метода из UniLibrary
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetLoggingAdvic: логированние попытки получить книгу/журнал");
+    @Before("aop_31_joint_point.MyPointcuts.allAddMethods()")
+    //аннотация выполнения Aspect логгирования до выполнение метода из UniLibrary
+    public void beforeAddLoggingAdvice(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("methodSignature = " + methodSignature);
+        System.out.println("methodSignature.getMethod() = " + methodSignature.getMethod());
+        System.out.println("methodSignature.getReturnType() = " + methodSignature.getReturnType());
+        System.out.println("methodSignature..getName() = " + methodSignature.getName());
+
+        System.out.println("beforeGetLoggingAdvice: логированние попытки получить книгу/журнал");
         System.out.println("----------------------------");
     }
 }
