82c5d44759e1cdce3f28bc3a8656c79a4a657d34
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-11 08:07:50 +0300

    Ad new files

--- src/main/java/aop_31_joint_point/MyPointcuts.java
+++ src/main/java/aop_31_joint_point/MyPointcuts.java
@@ -3,7 +3,7 @@ package aop_31_joint_point;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointcuts {
-    @Pointcut("execution(* get*())")
-    public void allGetMethods() {
+    @Pointcut("execution(* add*())")
+    public void allAddMethods() {
     }
 }
