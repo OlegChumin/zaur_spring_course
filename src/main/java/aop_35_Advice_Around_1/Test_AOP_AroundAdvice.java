50a39972ace4a03e1ba484962356c3c0c4079616
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-17 07:06:56 +0300

    Add new files

--- src/main/java/aop_35_Advice_Around_1/Test_AOP_AroundAdvice.java
+++ src/main/java/aop_35_Advice_Around_1/Test_AOP_AroundAdvice.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 public class Test_AOP_AroundAdvice {
     public static void main(String[] args) {
+        System.out.println("Method main starts..");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary library = context.getBean("uniLibrary", UniLibrary.class);
         String bookName = library.returnBook();
         System.out.println("В библиотеку вернули книгу " + bookName);
         context.close();
+        System.out.println("Method main ends...");
     }
 }
--- src/main/java/aop_35_Advice_Around_1/aspects/NewLoggingAspect.java
+++ src/main/java/aop_35_Advice_Around_1/aspects/NewLoggingAspect.java
@@ -1,5 +1,6 @@
 package aop_35_Advice_Around_1.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,14 @@ import org.springframework.stereotype.Component;
 public class NewLoggingAspect {
 
     @Around("execution(String returnBook())")
-    public void aroundReturnBookLoggingAdvice() {
-        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку возвращают книгу");
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку пытаются вернуть книгу");
+        long timeStarts = System.currentTimeMillis();
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        long timeEnds = System.currentTimeMillis();
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку успешно вернули книгу");
+        System.out.println("aroundReturnBookLoggingAdvice: метод returnBook выполнил работу за " +
+                (timeEnds - timeStarts) + " миллисекунд");
+        return targetMethodResult;
     }
 }
